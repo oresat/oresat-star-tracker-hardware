a626bbdcd1e05023ab8c8af8c887a0abffb110ff
rewsiffer <rewsiffer@gmail.com>
2018-02-16 23:28:10 -0800

    fdsfdsfsdf

---
+++ STM32F4-camera-system/stm32-camera/Debug/src/bmp.d
@@ -0,0 +1,37 @@
+src/bmp.o: ../src/bmp.c \
+ C:\Users\Oliver\Documents\SWSTM32_workspace\stm32-camera\Utilities\STM32F4-Discovery/stm32f4_discovery_lcd.h \
+ C:\Users\Oliver\Documents\SWSTM32_workspace\stm32-camera\CMSIS\Device\ST\STM32F4xx\Include/stm32f4xx.h \
+ C:\Users\Oliver\Documents\SWSTM32_workspace\stm32-camera\CMSIS\Include/core_cm4.h \
+ C:\Users\Oliver\Documents\SWSTM32_workspace\stm32-camera\CMSIS\Include/core_cmInstr.h \
+ C:\Users\Oliver\Documents\SWSTM32_workspace\stm32-camera\CMSIS\Include/core_cmFunc.h \
+ C:\Users\Oliver\Documents\SWSTM32_workspace\stm32-camera\CMSIS\Include/core_cm4_simd.h \
+ C:\Users\Oliver\Documents\SWSTM32_workspace\stm32-camera\CMSIS\Device\ST\STM32F4xx\Include/system_stm32f4xx.h \
+ C:\Users\Oliver\Documents\SWSTM32_workspace\stm32-camera\Utilities\STM32F4-Discovery/fonts.h \
+ C:\Users\Oliver\Documents\SWSTM32_workspace\stm32-camera\Utilities\FatFs_vR0.08a/ff.h \
+ C:\Users\Oliver\Documents\SWSTM32_workspace\stm32-camera\Utilities\FatFs_vR0.08a/integer.h \
+ C:\Users\Oliver\Documents\SWSTM32_workspace\stm32-camera\Utilities\FatFs_vR0.08a/ffconf.h \
+ C:\Users\Oliver\Documents\SWSTM32_workspace\stm32-camera\inc/main.h
+
+C:\Users\Oliver\Documents\SWSTM32_workspace\stm32-camera\Utilities\STM32F4-Discovery/stm32f4_discovery_lcd.h:
+
+C:\Users\Oliver\Documents\SWSTM32_workspace\stm32-camera\CMSIS\Device\ST\STM32F4xx\Include/stm32f4xx.h:
+
+C:\Users\Oliver\Documents\SWSTM32_workspace\stm32-camera\CMSIS\Include/core_cm4.h:
+
+C:\Users\Oliver\Documents\SWSTM32_workspace\stm32-camera\CMSIS\Include/core_cmInstr.h:
+
+C:\Users\Oliver\Documents\SWSTM32_workspace\stm32-camera\CMSIS\Include/core_cmFunc.h:
+
+C:\Users\Oliver\Documents\SWSTM32_workspace\stm32-camera\CMSIS\Include/core_cm4_simd.h:
+
+C:\Users\Oliver\Documents\SWSTM32_workspace\stm32-camera\CMSIS\Device\ST\STM32F4xx\Include/system_stm32f4xx.h:
+
+C:\Users\Oliver\Documents\SWSTM32_workspace\stm32-camera\Utilities\STM32F4-Discovery/fonts.h:
+
+C:\Users\Oliver\Documents\SWSTM32_workspace\stm32-camera\Utilities\FatFs_vR0.08a/ff.h:
+
+C:\Users\Oliver\Documents\SWSTM32_workspace\stm32-camera\Utilities\FatFs_vR0.08a/integer.h:
+
+C:\Users\Oliver\Documents\SWSTM32_workspace\stm32-camera\Utilities\FatFs_vR0.08a/ffconf.h:
+
+C:\Users\Oliver\Documents\SWSTM32_workspace\stm32-camera\inc/main.h:
