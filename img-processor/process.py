1ceeea8d3c1bd3095d1e5e1eaa78eb2557a9d40a
Umair Khan <hiflyer380@hotmail.com>
2018-07-04 17:56:56 -0700

    Image pre-processing based on black pixel analysis. Doesn't require 'ideal' image.

--- img-processor/process.py
+++ img-processor/process.py
@@ -2,11 +2,11 @@ import os
 import cv2
 import numpy as np
 
-# Open the ideal image
-ideal = cv2.imread("ideal.png", cv2.IMREAD_GRAYSCALE)
+# # Open the ideal image
+# ideal = cv2.imread("ideal.png", cv2.IMREAD_GRAYSCALE)
 
-# Count the number of black pixels in the ideal image
-ideal_black = (640 * 480) - cv2.countNonZero(ideal)
+# # Count the number of black pixels in the ideal image
+# ideal_black = (640 * 480) - cv2.countNonZero(ideal)
 
 # Go through all files in the sample directory
 for filename in os.listdir("samples"):
@@ -20,11 +20,14 @@ for filename in os.listdir("samples"):
 	# Count the number of black pixels in the sample image
 	sample_black = (640 * 480) - cv2.countNonZero(threshold)
 
-	# Calculate the percent difference
-	diff = (sample_black - ideal_black) / float(ideal_black)
+	# Print appropriate message
+	message = "good"
 
-	# Print results
-	print filename
-	print "\n\tIdeal : " + str(ideal_black)
-	print "\tSample: " + str(sample_black)
-	print "\tDiff  : " + str(diff) + "\n"
+	if sample_black > 307190:
+		message = "too few stars"
+	elif sample_black < 307000 and sample_black > 306900:
+		message = "too many stars"
+	elif sample_black <= 306900:
+		message = "not a pure star field"
+
+	print filename + ": " + str(sample_black) + ", " + message 
