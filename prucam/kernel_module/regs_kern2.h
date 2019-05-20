ede9a8d9d92eee63322dbc2cf09f48d3e5732183
Oliver Rew <oliver.rew@rigado.com>
2019-05-20 06:39:03 +0000

    cleaned up register file

--- prucam/kernel_module/regs_kern2.h
+++ prucam/kernel_module/regs_kern2.h
@@ -1,14 +1,3 @@
-/*
- * ===== regs.h =====
- * Define all sets of constant register sets here i.e.
- * startup register, sleep registers, etc.
- */
-
-/*
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-*/
 typedef struct
 {
   uint16_t reg;
@@ -16,111 +5,34 @@ typedef struct
 }camReg;
 
 camReg startupRegs[] = {
-  /*
-  {.reg = 0x301A, .val = 0x0001}, //RESET
-  {.reg = 0x301A, .val = 0x10D8}, //Disable Serial, Enable Parallel, Drive Outputs(no hi-z), lock reg
-  {.reg = 0x3086, .val = 0x0225}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x5050}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x2D26}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x0828}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x0D17}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x0926}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x0028}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x0526}, //SEQ RAM
-  {.reg = 0x3086, .val = 0xA728}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x0725}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x8080}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x2917}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x0525}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x0040}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x2702}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x1616}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x2706}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x1736}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x26A6}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x1703}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x26A4}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x171F}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x2805}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x2620}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x2804}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x2520}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x2027}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x0017}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x1E25}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x0020}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x2117}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x1028}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x051B}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x1703}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x2706}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x1703}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x1747}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x2660}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x17AE}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x2500}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x9027}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x0026}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x1828}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x002E}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x2A28}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x081E}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x0831}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x1440}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x4014}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x2020}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x1410}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x1034}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x1400}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x1014}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x0020}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x1400}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x4013}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x1802}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x1470}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x7004}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x1470}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x7003}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x1470}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x7017}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x2002}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x1400}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x2002}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x1400}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x5004}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x1400}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x2004}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x1400}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x5022}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x0314}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x0020}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x0314}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x0050}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x2C2C}, //SEQ RAM
-  {.reg = 0x3086, .val = 0x2C2C}, //SEQ RAM
-  {.reg = 0x309E, .val = 0x0000}, //DCDS_PROG_START_ADDR undocumented
-  {.reg = 0x30E4, .val = 0x6372}, //ADC_BITS_6_7 undocumented
+
+  //TODO should insert a reset here to clear registers
+
+  //Below are a bunch of register writes to largely undocumented registers that
+  //we pulled from the init sequence of an AR0130 sensor git repo. While I don't know
+  //what they mean, the outcome is the picture has a lot less noise afterwards.
+  //Likewise, the annotations suggest it is initing the ADCs and DACs. While,
+  //I don't love having a binary blob in here, it does improve the image
+  {.reg = 0x309E, .val = 0x0000}, //DCDS_PROG_START_ADDR
+  {.reg = 0x30E4, .val = 0x6372}, //ADC_BITS_6_7
   {.reg = 0x30E2, .val = 0x7253}, //ADC_BITS_4_5
   {.reg = 0x30E0, .val = 0x5470}, //ADC_BITS_2_3
   {.reg = 0x30E6, .val = 0xC4CC}, //ADC_CONFIG1
   {.reg = 0x30E8, .val = 0x8050}, //ADC_CONFIG2
-  //{.reg = 0x0000, .val = 1}, // delay 100 ms
-  //delay here
-  {.reg = 0x3082, .val = 0x0029}, //OP MODE CTL undocumented
-  {.reg = 0x301E, .val = 0x00C8}, // DATA_PEDESTAL
+  {.reg = 0x3082, .val = 0x0029}, //OP MODE CTL
+  {.reg = 0x301E, .val = 0x00C8}, // DATA_PEDESTAL documented
   {.reg = 0x3EDA, .val = 0x0F03}, // DAC_LD_14_15
   {.reg = 0x3EDE, .val = 0xC005}, // DAC_LD_18_19
   {.reg = 0x3ED8, .val = 0x09EF}, // DAC_LD_12_13
   {.reg = 0x3EE2, .val = 0xA46B}, // DAC_LD_22_23
   {.reg = 0x3EE0, .val = 0x047D}, // DAC_LD_20_21
   {.reg = 0x3EDC, .val = 0x0070}, // DAC_LD_16_17
-  {.reg = 0x3044, .val = 0x0404}, // DARK_CONTROL //DEFAULT
+  {.reg = 0x3044, .val = 0x0404}, // DARK_CONTROL documented
   {.reg = 0x3EE6, .val = 0x4303}, // DAC_LD_26_27
   {.reg = 0x3EE4, .val = 0xD208}, // DAC_LD_24_25
   {.reg = 0x3ED6, .val = 0x00BD}, // DAC_LD_10_11
-  */
-
-  //==========START NEW REGS
+  //END BINARY BLOB
+  
   {.reg = 0x3028, .val = 0x0010}, //row_speed: Dout on rising edge pixclk DEFAULT
 
   //Pixel clock settings:
@@ -151,33 +63,12 @@ camReg startupRegs[] = {
   {.reg = 0x3014, .val = 0x00C0}, 
 
   {.reg = 0x30A6, .val = 0x0001}, //y_odd_inc: skip no rows
-
-  {.reg = 0x3046, .val = 0x0100}, //flash: enable flash signal, helps verify exposure
-
-  //===CONTEXT B STUFF, DONT THINK I NEED THIS NOW
-  /*
-     {.reg = 0x308C, .val = 0x0000}, //y_addr_start_cb
-     {.reg = 0x308A, .val = 0x0000}, //x_addr_start_cb
-     {.reg = 0x3090, .val = 0x03BF}, //y_addr_end_cd
-     {.reg = 0x308E, .val = 0x04FF}, //x_addr_end_cb
-     {.reg = 0x30AA, .val = 0x03DE}, //frame_length_lines_cb
-     {.reg = 0x3016, .val = 0x0064}, //course_integration_time_cb
-     {.reg = 0x3018, .val = 0x00C0}, //fine_integration_time_cb
-     {.reg = 0x30A8, .val = 0x0001}, //y_odd_inc_cb
-  //==============================================
-  */
-
   {.reg = 0x3040, .val = 0x0000}, //read_mode: normal readout mode DEFAULT
   {.reg = 0x3064, .val = 0x1802}, //embedded_data_control: Embedded data disabled
   {.reg = 0x31C6, .val = 0x8000}, //hispi_control_status: all HISPI options off: DEFAULT
 
-  //GAIN: TODO not sure these make sense for mono, try defaults
-  /*
-     {.reg = 0x3056, .val = 0x0030}, //green1_gain: 1.5 green gain
-     {.reg = 0x3058, .val = 0x0042}, //blue_gain: 2.0625 blue gain
-     {.reg = 0x305A, .val = 0x0042}, //reg_gain: 2.0625 reg gain
-     {.reg = 0x305C, .val = 0x0030}, //green2_gain: 1.5 green gain
-     */
+  {0, 0}, //blank entry to terminate array 
+};
 
   /*
      {.reg = 0x301A, .val = 0x0001}, //RESET
@@ -307,6 +198,3 @@ camReg startupRegs[] = {
 
   {.reg = 0x301A, .val = 0x10DC},  //Disable Serial, Enable Parallel, Drive Outputs(no hi-z), lock reg, streaming mode(not low power)
   */	
-  {0, 0}, //blank entry to terminate array 
-};
-
