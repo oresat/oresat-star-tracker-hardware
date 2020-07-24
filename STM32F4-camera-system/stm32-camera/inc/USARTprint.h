c32dcecb2f534a044b04eb23aacb0b702d2c6e2c
Oliver Rew <oliver.rew@rigado.com>
2020-07-23 19:14:11 -0700

    removed STM32 camera code that we haven't used in 2 years

--- STM32F4-camera-system/stm32-camera/inc/USARTprint.h
+++
@@ -1,5 +0,0 @@
-//headers for USART print
-#include <stm32f4xx_usart.h> // under Libraries/STM32F4xx_StdPeriph_Driver/inc and src
-
-#define MAX_STRLEN 12 // this is the maximum string length of our string in characters
-volatile char received_string[MAX_STRLEN+1]; // this will hold the recieved string
